b067df93e29bb7a6597370e8da585c783db5e245
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-13 17:31:47 -0600

    Added cors to the server.js file and made it to where the React app is in a clean state ready to create components

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const helmet = require('helmet');
+const cors = require('cors');
 
 const postRouter = require('./posts/posts-router.js');
 const userRouter = require('./users/users-router.js');
@@ -8,6 +9,7 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 
 server.use('/api/posts', postRouter);
 server.use('/api/users', userRouter);
