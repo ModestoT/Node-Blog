19692e208579fb5b5c288783de6a548452ed3504
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-13 15:42:37 -0600

    Added end-point for added a post to the database

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -29,4 +29,46 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', userId(), async (req, res) => {
+    try {
+        const newPost = { ...req.body, user_id: req.headers.userid};
+        const post = await Posts.insert(newPost);
+        
+        if(post){
+            res.status(201).json(post);
+        } else {
+            res.status(404).json({ error: 'that userId does not exist'});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "There was an error while saving the post to the database" });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const postId = await Posts.remove(req.params.id);
+
+        if(postId){
+            res.status(200).json({ errorMessage: 'Post deleted' });
+        } else {
+            res.status(404).json({ errorMessage: "The post with the specified ID does not exist." });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "The post could not be removed" });
+    }
+});
+
+function userId() {
+    return function(req, res, next){
+        const userId = req.headers.userid;
+        if(userId){
+            next();
+        } else {
+            res.status(400).json({ message: 'Please login to preform this action' });
+        }
+    };
+}
+
 module.exports = router;
